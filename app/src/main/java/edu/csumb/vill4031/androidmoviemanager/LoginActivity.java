3873c44ba2cbe04156f25bb254e2ce767fcdd17f
razrow <razrow@gmail.com>
2020-11-20 16:38:26 -0800

    Got the UI to work with Joseph's changes.

--- app/src/main/java/edu/csumb/vill4031/androidmoviemanager/LoginActivity.java
+++ app/src/main/java/edu/csumb/vill4031/androidmoviemanager/LoginActivity.java
@@ -3,12 +3,50 @@ package edu.csumb.vill4031.androidmoviemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if(ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "btnLogin was clicked");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username,password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
