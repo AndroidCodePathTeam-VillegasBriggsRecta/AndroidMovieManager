c4b6ad50d6598f8a1a48bf86783d5301b9c4e998
Joseph-Villegas <jovillegas@csumb.edu>
2020-12-03 21:47:23 -0800

    updated movie detail page, layout filled

--- app/src/main/java/edu/csumb/vill4031/androidmoviemanager/fragments/MovieFragment.java
+++ app/src/main/java/edu/csumb/vill4031/androidmoviemanager/fragments/MovieFragment.java
@@ -2,6 +2,8 @@ package edu.csumb.vill4031.androidmoviemanager.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Parcelable;
@@ -9,6 +11,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 import org.parceler.Parcels;
 
@@ -41,12 +50,6 @@ public class MovieFragment extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static MovieFragment newInstance(Movie param1) {
-//        MovieFragment fragment = new MovieFragment();
-//        Bundle args = new Bundle();
-//        args.putParcelable(ARG_PARAM1, (Parcelable) param1);
-//        fragment.setArguments(args);
-        //return fragment;
-
         MovieFragment fragment = new MovieFragment();
         Bundle args = new Bundle();
         args.putSerializable("movie", param1);
@@ -58,11 +61,9 @@ public class MovieFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            //mParam1 = getArguments().getParcelable(ARG_PARAM1);
-           mParam1 = (Movie) getArguments().getSerializable("movie");
+            mParam1 = (Movie) getArguments().getSerializable("movie");
             Log.i(TAG, "Releases: " + mParam1.getTitle());
         }
-
     }
 
     @Override
@@ -70,5 +71,41 @@ public class MovieFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie, container, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView ivPoster = view.findViewById(R.id.ivPoster);
+        TextView tvTitle = view.findViewById(R.id.tvTitle);
+        RatingBar ratingBar = view.findViewById(R.id.ratingBar);
+        TextView tvOverview = view.findViewById(R.id.tvOverview);
+
+        String imageUrl = mParam1.getPosterPath();
+        Glide.with(getContext()).load(imageUrl).into(ivPoster);
+
+        tvTitle.setText(mParam1.getTitle());
+        ratingBar.setRating((float) mParam1.getRating());
+        tvOverview.setText(mParam1.getOverview());
+
+        Button catalogBtn = view.findViewById(R.id.catalogBtn);
+        Button wishlistBtn = view.findViewById(R.id.wishlistBtn);
+
+        catalogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Adding to Catalog", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        wishlistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Adding to Wishlist", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
