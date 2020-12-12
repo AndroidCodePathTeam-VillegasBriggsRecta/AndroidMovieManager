1cc46fd3a96128aea03c153151a48737263638a1
Joseph-Villegas <jovillegas@csumb.edu>
2020-12-11 20:01:01 -0800

    toogle catalog/wishlist in list fragment

--- app/src/main/java/edu/csumb/vill4031/androidmoviemanager/fragments/ListFragment.java
+++ app/src/main/java/edu/csumb/vill4031/androidmoviemanager/fragments/ListFragment.java
@@ -10,6 +10,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -56,7 +59,15 @@ public class ListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        final Switch simpleSwitch = (Switch) view.findViewById(R.id.listSwitch);
+
+        final TextView tvListName = view.findViewById(R.id.tvListName);
+
         RecyclerView rvMovies = view.findViewById(R.id.rvMovieList);
+
+        simpleSwitch.setText("Catalog");
+
+        tvListName.setText("Catalog");
 
         catalog = new ArrayList<>();
 
@@ -69,21 +80,77 @@ public class ListFragment extends Fragment {
         // Set a Layout Manager
         rvMovies.setLayoutManager(new LinearLayoutManager(getContext()));
 
-         catalog.addAll(fetchCatalog());
-         movieAdapter.notifyDataSetChanged();
+        catalog.addAll(fetchCatalog());
+        tvListName.setText(String.format("%s Movies in Catalog", catalog.size()));
+        movieAdapter.notifyDataSetChanged();
 
         Log.i(TAG, "CATALOG SIZE: " + String.valueOf(catalog.size()));
 
-        for (Movie movie : catalog) {
-            Log.i(TAG, movie.getTitle());
-        }
-
+        simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    Log.i(TAG, "show wish list");
+                    simpleSwitch.setText("Wish List");
+                    catalog.clear();
+                    catalog.addAll(fetchWishList());
+                    tvListName.setText(String.format("%s Movies in Wish List", catalog.size()));
+                } else {
+                    Log.i(TAG, "not checked!");
+                    simpleSwitch.setText("Catalog");
+                    catalog.clear();
+                    catalog.addAll(fetchCatalog());
+                    tvListName.setText(String.format("%s Movies in Catalog", catalog.size()));
+                }
+                movieAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private List<Movie> fetchCatalog() {
         List<Movie> movies = new ArrayList<>();
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Catalog");
+        query.whereEqualTo("user_id", ParseUser.getCurrentUser());
+
+        ParseQuery<ParseMovie> movieQuery = ParseQuery.getQuery("Movie");
+
+        try {
+            List<ParseObject> results = query.find();
+            for (ParseObject result : results) {
+                ParseObject obj = (ParseObject) result.get("movie_id");
+
+                try {
+                    List<ParseMovie> res = movieQuery.find();
+                    for (ParseMovie r : res) {
+                        if (r.getObjectId().equals(obj.getObjectId())) {
+                            Movie m = new Movie();
+
+                            m.setMovieId(r.getIMDbID());
+                            m.setBackdropPath(r.getBackdropPath());
+                            m.setOverview(r.getDescription());
+                            m.setRating(r.getRating());
+                            m.setPosterPath(r.getPosterPath());
+                            m.setTitle(r.getTitle());
+
+                            movies.add(m);
+                        }
+                    }
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return movies;
+    }
+
+    private List<Movie> fetchWishList() {
+        List<Movie> movies = new ArrayList<>();
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Wish_List");
         query.whereEqualTo("user_id", ParseUser.getCurrentUser());
 
         ParseQuery<ParseMovie> movieQuery = ParseQuery.getQuery("Movie");
